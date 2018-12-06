31db8f3646d4e6578f6592076616e9595963f5a8
AndreiMihalea <mihalea.andrei@gmail.com>
2018-12-06 14:18:21 -0800

    Added problem 6

---
+++ 6-xor-linked-list/xor-linked-list.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h> 
+#include <iostream>
+using namespace std;
+
+typedef struct Node
+{
+    int data;
+    struct Node *xr;
+} Node;
+
+Node *head, *tail;
+
+Node* XOR(Node *a, Node *b)
+{
+    return (Node*) ((uintptr_t)(a) ^ (uintptr_t)(b));
+}
+
+Node* insert(int data)
+{
+    cout << data << endl;
+    if (tail == NULL) {
+        tail = new Node;
+        tail->data = data;
+        tail->xr = NULL;
+    } else {
+        Node *new_node = new Node;
+        new_node->data = data;
+        new_node->xr = XOR(tail, NULL);
+        tail->xr = XOR(new_node, XOR(tail->xr, NULL));
+        tail = new_node;
+    }
+
+    if (head == NULL) {
+        head = tail;
+    }
+}
+
+void list()
+{
+    Node *curr = head;
+    Node *prev = NULL, *next;
+
+    while (NULL != curr) {
+        cout << curr->data << " ";
+        next = XOR(prev, curr->xr);
+        prev = curr;
+        curr = next;
+    }
+
+    cout << endl;
+}
+
+Node *get(int index)
+{
+    Node *curr = head;
+    Node *prev = NULL, *next;
+
+    while (NULL != curr && index > 0) {
+        next = XOR(prev, curr->xr);
+        prev = curr;
+        curr = next;
+        index--;
+    }
+
+    return curr;
+}
+
+int main()
+{
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(40); 
+
+    list();
+
+    cout << get(0)->data;
+  
+    return 0; 
+}
